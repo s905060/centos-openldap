635aaf3d643a00d2296c439539d40467d155711b
Jash Lee <jash.lee@nielsen.com>
2015-07-28 17:30:45 -0400

    V5

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ if [[ ! -f /var/lib/ldap/DB_CONFIG ]]; then
   cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
 fi
 
-chown -R ldap:ldap /var/lib/ldap /etc/openldap/slapd.d
+chown -R root:root /var/lib/ldap /etc/openldap/slapd.d
 
 if [[ ! -z "$MAX_NOFILE" ]]; then
   ulimit -n "$MAX_NOFILE"
 fi
 
-exec slapd -d 3000 -u ldap -g ldap -h "$SLAPD_URL"
+exec slapd -d 3000 -u root -g root -h "$SLAPD_URL"
